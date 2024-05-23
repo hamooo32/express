f6ca0bbfbf4dd1cf3d12b5fa67f8eeb84c9561a1
hamooo32 <169149891+hamooo32@users.noreply.github.com>
2024-05-23 09:17:13 +0100

    Update Article.js

--- models/Article.js
+++ models/Article.js
@@ -2,8 +2,12 @@
 const mongoose = require("mongoose")
 const Schema = mongoose.Schema
 const articleSchema = new Schema({
-    title:String,
-    body:String
+    title:{
+        type:String
+    },
+    body:{
+        type:String
+    }
 })
 const article =mongoose.model("Article",articleSchema)
 module.exports = article
