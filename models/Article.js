044450005256b25249206c9f6be91765cda6f5d2
hamooo32 <169149891+hamooo32@users.noreply.github.com>
2024-05-23 18:23:50 +0100

    Update Article.js

--- models/Article.js
+++ models/Article.js
@@ -2,10 +2,10 @@
 const mongoose = require("mongoose")
 const Schema = mongoose.Schema
 const articleSchema = new Schema({
-    title:{
+    username:{
         type:String
     },
-    body:{
+    password:{
         type:String
     }
 })
