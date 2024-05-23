cf0e95111611083dd21ac9d851a5b20f9f111c0c
hamooo32 <169149891+hamooo32@users.noreply.github.com>
2024-05-23 17:57:45 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const mongoose = require("mongoose")
 const Article = require("./models/Article.js")
 
 const app = express()
+const cors = require('cors')
+app.use(cors());
 mongoose.connect("mongodb+srv://hamooo32:hamooogaming32@my-express.55wvrwz.mongodb.net/?retryWrites=true&w=majority&appName=my-express")
 .then(()=>{
     console.log("Connected!")
@@ -11,41 +13,8 @@ mongoose.connect("mongodb+srv://hamooo32:hamooogaming32@my-express.55wvrwz.mongo
     console.log("ERROR:"+err)
 })
 app.use(express.json())
-app.get('/',(req,res)=>{
-    res.send("welcome")
-})
-app.get('/hello/:num1/:num2',(req,res)=>{
-    let param =req.params
-    let x = Number(req.params.num1) + Number(req.params.num2)
-    console.log(x)
-    res.send(String(x))
-})
-app.get('/hello2',(req,res)=>{
-    res.render("index.ejs",{
-        name:"ali"
-    })
-})
-app.post('/article', async (req,res)=>{
-    let N = new Article(req.body);
-    await N.save()
-    console.log("DONE!")
-    console.log(req.body)
-    res.send(req.body)
-})
-app.get('/article/:a',async(req,res)=>{
-    console.log(req.params.a)
-    let x =await Article.findById(req.params.a)
-    res.json(x)
-})
-app.get('/article',async(req,res)=>{
-    let x =await Article.find()
-    res.json(x)
-})
-app.get('/articl/:a',async(req,res)=>{
-    let x =await Article.find()
-    res.json(x[1])
-})
 
-app.listen(3000,()=>{
+
+app.listen(3001,()=>{
     console.log("Done");
 })
